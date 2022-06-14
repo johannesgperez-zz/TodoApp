771b01c7c6b0dc962302e8dfb118eff59a3a2f56
johannes <johannesgperez@gmail.com>
2022-06-13 23:12:25 -0300

    More functionalities

--- base/models.py
+++ base/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         ordering = ['completed']
--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from base.views import TaskDelete, TaskDetail, TaskList, TaskCreate, TaskUpdate, LoginView
+from base.views import TaskDelete, TaskDetail, TaskList, TaskCreate, TaskUpdate, LoginPage, RegisterPage
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task_list'),
@@ -8,6 +8,9 @@ urlpatterns = [
     path('task_create/', TaskCreate.as_view(), name='task_create'),
     path('task_update/<int:pk>/', TaskUpdate.as_view(), name='task_update'),
     path('task_delete/<int:pk>/', TaskDelete.as_view(), name='task_delete'),
-    path('login/', LoginView.as_view(), name='login'),
+    
+    path('login/', LoginPage.as_view(), name='login'),
     path('logout/', LogoutView.as_view(template_name='base/logout.html'), name='logout'),
+    
+    path('register/', RegisterPage.as_view(), name='register'),
 ]
