0a97af53f5bf3d6d3b9331aa9ba2236d81de1091
johannes <johannesgperez@gmail.com>
2022-06-29 22:07:57 -0300

    some css changes

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -122,9 +122,6 @@ LOGIN_URL = '/login/'
 
 STATIC_URL = 'base/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'base/static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
